8f71fdc5b6b22a25530c357f2604bfeec6f30709
hryoshihara0692 <hr.yoshihara0692@gmail.com>
2024-05-22 01:15:59 +0900

    2024/05/22 Flutter初期表示から、テキストのみ表示に変更

---
+++ lib/pages/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Home');
+  }
+}
